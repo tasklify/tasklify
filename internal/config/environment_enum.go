b4012705a631822eb4dc05379828ee3dd1102e8e
mk2376 <mk2376@student.uni-lj.si>
2024-03-06 21:38:44 +0100

    Fix typos

--- internal/config/environment_enum.go
+++ internal/config/environment_enum.go
@@ -9,17 +9,17 @@ import (
 type Environment enum.Member[string]
 
 var (
-	e            = enum.NewBuilder[string, Environment]()
-	Prod         = e.Add(Environment{"prod"})
-	Dev          = e.Add(Environment{"dev"})
-	Environments = e.Enum()
+	e               = enum.NewBuilder[string, Environment]()
+	EnvironmentProd = e.Add(Environment{"prod"})
+	EnvironmentDev  = e.Add(Environment{"dev"})
+	Environments    = e.Enum()
 )
 
 // Only for parsers
 func environmentParser(v string) (interface{}, error) {
 	env := Environments.Parse(v)
 	if env == nil {
-		return nil, fmt.Errorf("'%s' is not a valid enviranment enum", v)
+		return nil, fmt.Errorf("'%s' is not a valid environment enum", v)
 	}
 	return *env, nil
 }
