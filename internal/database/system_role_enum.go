8682bd831784223763d86208d8607ab90991787c
mk2376 <mk2376@student.uni-lj.si>
2024-03-08 19:13:53 +0100

    Fix enum and Gorm Value() function overlap

--- internal/database/system_role_enum.go
+++ internal/database/system_role_enum.go
@@ -16,13 +16,24 @@ var (
 	SystemRoles     = e.Enum()
 )
 
-func (role SystemRole) Value() (driver.Value, error) {
-	fmt.Println("here1")
-	return driver.Value(SystemRoles.Value(role)), nil
+func (role *SystemRole) Enum() enum.Enum[SystemRole, string] {
+	return SystemRoles
 }
 
-func (role *SystemRole) Scan(value interface{}) error {
+// CustomType embeds SystemRole and implements driver.Valuer
+type SystemRoleGORM struct {
+	*SystemRole
+}
+
+func (role *SystemRoleGORM) Value() (driver.Value, error) {
+	fmt.Println("here1")
+	return driver.Value(SystemRoles.Value(*role.SystemRole)), nil
+}
+
+func (role *SystemRoleGORM) Scan(value interface{}) error {
 	fmt.Println("here2")
-	*role = *SystemRoles.Parse(value.(string))
+	systemRole := SystemRoles.Parse(value.(string))
+
+	*role = SystemRoleGORM{SystemRole: systemRole}
 	return nil
 }
