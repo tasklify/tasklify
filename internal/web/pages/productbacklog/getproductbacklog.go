300cb587da7ac87261bec79c32b819a1388e9d5d
Tjasa Domadenik <tjasa.domadenik10@gmail.com>
2024-03-12 10:26:59 +0100

    Additions to handler, implemented user story fetching for product backlog.

--- internal/web/pages/productbacklog/getproductbacklog.go
+++ internal/web/pages/productbacklog/getproductbacklog.go
@@ -2,6 +2,7 @@ package productbacklog
 
 import (
 	"net/http"
+	"strconv"
 	"tasklify/internal/database"
 	"tasklify/internal/handlers"
 	"tasklify/internal/web/pages"
@@ -17,19 +18,46 @@ func GetProductBacklog(w http.ResponseWriter, r *http.Request, params handlers.R
 	}
 
 	// unassigned, unrealized user stories
-	var unassignedUnrealizedUs = filterUnassignedUnrealized(userStories)
+	var usInBacklog, usInSprint = filterBacklog(userStories)
 
-	c := productBacklog(unassignedUnrealizedUs)
+	var userStoriesBySprint = groupUserStoriesBySprint(usInSprint)
+	c := productBacklog(usInBacklog, userStoriesBySprint)
 
 	return pages.Layout(c, "My website").Render(r.Context(), w)
 }
 
-func filterUnassignedUnrealized(userStories []database.UserStory) (ret []database.UserStory) {
+func filterBacklog(userStories []database.UserStory) (inBacklog []database.UserStory, inSprint []database.UserStory) {
 
 	for _, us := range userStories {
-		if us.UserID == 0 && *us.Realized == false { // TODO check if works
-			ret = append(ret, us)
+		if us.UserID == 0 && *us.Realized == false && *us.SprintID == 0 {
+			inBacklog = append(inBacklog, us)
+		} else {
+			inSprint = append(inSprint, us)
 		}
 	}
+	return
+}
+
+func groupUserStoriesBySprint(userStories []database.UserStory) (userStoriesBySprint map[string][]database.UserStory) {
+
+	userStoriesBySprint = make(map[string][]database.UserStory)
+
+	for _, us := range userStories {
+
+		var sprintId = strconv.FormatUint(uint64(*us.SprintID), 10)
+
+		if val, ok := userStoriesBySprint[sprintId]; ok {
+			// add to existing slice
+			var usSlice = val
+			usSlice = append(usSlice, us)
+			userStoriesBySprint[sprintId] = usSlice
+
+		} else {
+			// create a new slice
+			sprintUserStories := []database.UserStory{us}
+			userStoriesBySprint[sprintId] = sprintUserStories
+		}
+	}
+
 	return
 }
