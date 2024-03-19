955cd9d9c3b350bfd05eb020ca1b985b40fc06bf
Lena Trnovec <115463806+lenatr99@users.noreply.github.com>
2024-03-19 11:43:00 +0100

    Added logout button and fixed findbyusername (#43)

---
+++ internal/web/pages/logout/postlogout.go
@@ -0,0 +1,25 @@
+package logout
+
+import (
+	"log"
+	"net/http"
+	"tasklify/internal/auth"
+)
+
+func PostLogout(w http.ResponseWriter, r *http.Request) error {
+    if r.Method != http.MethodPost {
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+        return nil
+	}
+
+	err := auth.GetSession().Destroy(w, r)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return nil
+	}
+
+	w.Header().Set("HX-Redirect", "/")
+	w.WriteHeader(http.StatusOK)
+	return nil
+}
