a457037d9498118ff21c454c47579fededc39dab
lenatr99 <lt89715@student.uni-lj.si>
2024-03-24 19:34:21 +0100

    changed sprint backlog layout

--- internal/web/pages/sprintbacklog/getsprintbacklog.go
+++ internal/web/pages/sprintbacklog/getsprintbacklog.go
@@ -48,16 +48,28 @@ func GetUserFirstAndLastNameFromID(userID uint) string {
 func mapTasksToStatuses(tasks []database.Task) (statusMap map[string][]database.Task) {
 	statusMap = make(map[string][]database.Task)
 	for _, task := range tasks {
-		if (task.UserID == nil) || !*task.UserAccepted {
+		if (task.UserID == nil) {
 			statusMap["Unassigned"] = append(statusMap["Unassigned"], task)
 		} else {
-			if *task.Status == database.StatusInProgress {
+			if !*task.UserAccepted {
+				statusMap["Pending"] = append(statusMap["Pending"], task)
+			} else if *task.Status == database.StatusInProgress {
 				statusMap["Active"] = append(statusMap["Active"], task)
 			} else if *task.Status == database.StatusDone {
 				statusMap["Done"] = append(statusMap["Done"], task)
 			} else {
 				statusMap["Assigned"] = append(statusMap["Assigned"], task)
 			}
+		}
+	}
+	return
+}
+
+ func GetSumOfTimeEstimates(tasks []database.Task) (sum float32) {
+	sum = 0
+	for _, task := range tasks {
+		if task.TimeEstimate != nil {
+			sum += *task.TimeEstimate
 		}
 	}
 	return
