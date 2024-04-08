ac33bff013d575d3834a39b6ad42a42c71e4b71f
Anže Oblak <ao7713@student.uni-lj.si>
2024-04-08 21:06:00 +0200

    Fixed tabs selection

--- internal/web/pages/userstory/DetailsUserStory.go
+++ internal/web/pages/userstory/DetailsUserStory.go
@@ -1,15 +1,19 @@
 package userstory
 
 import (
-	"github.com/go-chi/chi/v5"
 	"net/http"
 	"strconv"
 	"tasklify/internal/database"
+
+	"github.com/go-chi/chi/v5"
 
 	"tasklify/internal/handlers"
 )
 
 func GetUserStoryDetails(w http.ResponseWriter, r *http.Request, params handlers.RequestParams) error {
+	type RequestData struct {
+		TabID *string `schema:"tabID"`
+	}
 
 	if err := r.ParseForm(); err != nil {
 		return err
@@ -21,12 +25,25 @@ func GetUserStoryDetails(w http.ResponseWriter, r *http.Request, params handlers
 		return err
 	}
 
+	var requestData RequestData
+	err = decoder.Decode(&requestData, r.URL.Query())
+	if err != nil {
+		return err
+	}
+
+	var activeTab string
+	if requestData.TabID == nil {
+		activeTab = "details"
+	} else {
+		activeTab = *requestData.TabID
+	}
+
 	userStory, err := database.GetDatabase().GetUserStoryByID(uint(userStoryID))
 	if err != nil {
 		return err
 	}
 
-	c := UserStoryDetailsDialog(*userStory)
+	c := UserStoryDetailsDialog(*userStory, activeTab)
 	return c.Render(r.Context(), w)
 
 }
