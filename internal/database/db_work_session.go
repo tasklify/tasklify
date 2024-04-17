52712f1c3c230b61220ca281680582540068b5d9
anzeoblak <73539255+anzeo@users.noreply.github.com>
2024-04-17 12:18:08 +0200

    Feat: finish task (#117)
    * Finish tasks
    
    * Added tooltip text

--- internal/database/db_work_session.go
+++ internal/database/db_work_session.go
@@ -8,14 +8,93 @@ import (
 
 type WorkSession struct {
 	gorm.Model
-	StartTime    time.Time
-	EndTime      *time.Time
-	Duration     time.Duration
-	Remaining    time.Duration
-	TaskID       uint
-	UserID       uint
-	OngoingToday bool
+	StartTime      time.Time
+	EndTime        *time.Time
+	Duration       time.Duration
+	Remaining      time.Duration
+	TaskID         uint
+	UserID         uint
+	OngoingToday   bool
 	LeftUnfinished bool
+}
+
+func (ws *WorkSession) AfterUpdate(tx *gorm.DB) (err error) {
+	var workSessions = []WorkSession{}
+	err = tx.Order("start_time asc").Find(&workSessions, "task_id = ?", ws.TaskID).Error
+	if err != nil {
+		return err
+	}
+
+	todaysWS := []WorkSession{}
+	otherWS := []WorkSession{}
+	for _, session := range workSessions {
+		if session.OngoingToday {
+			todaysWS = append(todaysWS, session)
+		} else {
+			otherWS = append(otherWS, session)
+		}
+	}
+
+	task, err := databaseClient.GetTaskByID(ws.TaskID)
+	if err != nil {
+		return err
+	}
+
+	if (len(todaysWS) != 0 && todaysWS[0].Remaining == 0) || (len(todaysWS) == 0 && len(otherWS) != 0 && otherWS[len(otherWS)-1].Remaining == 0) {
+		task.Status = &StatusDone
+	} else {
+		task.Status = &StatusInProgress
+	}
+
+	if err = databaseClient.UpdateTask(task); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ws *WorkSession) BeforeDelete(tx *gorm.DB) (err error) {
+	// Only data we provided on delete is available in ws, so get the whole object first
+	err = tx.First(&ws, ws.ID).Error
+	if err != nil {
+		return err
+	}
+
+	// Fetch all work sessions but the one to be deleted
+	var workSessions = []WorkSession{}
+	err = tx.Order("start_time asc").Where("id != ?", ws.ID).Find(&workSessions, "task_id = ?", ws.TaskID).Error
+	if err != nil {
+		return err
+	}
+
+	todaysWS := []WorkSession{}
+	otherWS := []WorkSession{}
+	for _, session := range workSessions {
+		if session.OngoingToday {
+			todaysWS = append(todaysWS, session)
+		} else {
+			otherWS = append(otherWS, session)
+		}
+	}
+
+	task, err := databaseClient.GetTaskByID(ws.TaskID)
+	if err != nil {
+		return err
+	}
+
+	if (len(todaysWS) != 0 && todaysWS[0].Remaining == 0) || (len(todaysWS) == 0 && len(otherWS) != 0 && otherWS[len(otherWS)-1].Remaining == 0) {
+		task.Status = &StatusDone
+	} else if len(todaysWS) == 0 && len(otherWS) == 0 {
+		task.Status = &StatusTodo
+	} else {
+		task.Status = &StatusInProgress
+	}
+
+	if err = databaseClient.UpdateTask(task); err != nil {
+		return err
+	}
+
+	return
 }
 
 func (db *database) GetWorkSessionByID(sessionID uint) (*WorkSession, error) {
@@ -38,22 +117,22 @@ func (db *database) CreateWorkSessionToday(userID, taskID uint) error {
 		return err
 	}
 	session := WorkSession{
-		StartTime:    time.Now(),
-		TaskID:       taskID,
-		UserID:       userID,
-		OngoingToday: true,
+		StartTime:      time.Now(),
+		TaskID:         taskID,
+		UserID:         userID,
+		OngoingToday:   true,
 		LeftUnfinished: false,
-		Remaining: Remaining,
+		Remaining:      Remaining,
 	}
 	return db.Create(&session).Error
 }
 
 func (db *database) GetRemainingTimeForTask(taskID uint) (time.Duration, error) {
-    var sessions []WorkSession
-    err := db.Order("start_time asc").Find(&sessions, "task_id = ?", taskID).Error
-    if err != nil {
-        return 0, err
-    }
+	var sessions []WorkSession
+	err := db.Order("start_time asc").Find(&sessions, "task_id = ?", taskID).Error
+	if err != nil {
+		return 0, err
+	}
 
 	task, err := db.GetTaskByID(taskID)
 	if err != nil {
@@ -61,15 +140,14 @@ func (db *database) GetRemainingTimeForTask(taskID uint) (time.Duration, error) 
 	}
 
 	var defaultValue = task.TimeEstimate
-    for _, session := range sessions {
-        if !session.OngoingToday {
-            defaultValue = session.Remaining
-        }
-    }
+	for _, session := range sessions {
+		if !session.OngoingToday {
+			defaultValue = session.Remaining
+		}
+	}
 
 	return defaultValue, nil
 }
-
 
 func (db *database) UpdateWorkSession(session *WorkSession) error {
 	return db.Save(session).Error
@@ -86,5 +164,5 @@ func (db *database) GetWorkSessionsForTask(taskID uint) ([]WorkSession, error) {
 }
 
 func (db *database) DeleteWorkSession(sessionID uint) error {
-	return db.Unscoped().Where("id = ?", sessionID).Delete(&WorkSession{}).Error
+	return db.Unscoped().Where("id = ?", sessionID).Delete(&WorkSession{Model: gorm.Model{ID: sessionID}}).Error
 }
